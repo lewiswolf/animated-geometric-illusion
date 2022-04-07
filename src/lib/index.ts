b6571c7bd69bebca09d0deb754b6cb69bbaff10a
lewiswolf <lewiswolstanholme@gmail.com>
2022-04-07 15:34:55 +0100

    moved WA comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,6 @@ export async function importGeometry(): Promise<Geometry> {
 	const res = await WebAssembly.instantiateStreaming(fetch('./geometry.wasm'), {})
 	return res.instance.exports
 }
+
+// const geometry: Geometry = await importGeometry()
+// geometry?.add !== undefined && console.log(geometry.add!(4.3, 3.8))
