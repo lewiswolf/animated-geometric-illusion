c2ad10b46aaa902e84aea7b9d46705f7e9144d9f
lewiswolf <lewiswolstanholme@gmail.com>
2022-04-11 18:21:26 +0100

    deprecated wasm

--- src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-import { Geometry } from './types'
-
-export async function importGeometry(): Promise<Geometry> {
-	const res = await WebAssembly.instantiateStreaming(fetch('./geometry.wasm'), {})
-	return res.instance.exports
-}
-
-// const geometry: Geometry = await importGeometry()
-// geometry?.add !== undefined && console.log(geometry.add!(4.3, 3.8))
--- src/lib/types.d.ts
+++
@@ -1,3 +0,0 @@
-export interface Geometry extends WebAssembly.Exports {
-	add?(a: number, b: number): number
-}
